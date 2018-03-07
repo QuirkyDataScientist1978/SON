9d3f2d8c08c6a59c122a8700516f42054f7e32dc
Athanasios Salamanis <asal@iti.gr>
2018-03-07 11:39:26 +0200

    Made JPanel showing a live clustering. Added algorithm's properties in a file (parameters.csv). Added a runnable JAR.

--- src/SON.java
+++ src/SON.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 
 import java.io.*;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 public class SON
 {
@@ -15,16 +16,29 @@ public class SON
 	int dimension;
 	double lr;
 	int epochs;
+	int delayTime;
 	ArrayList<Neuron> neurons;
 	ArrayList<Point> points;
 	HashMap<Integer, Integer> classificationMatrix;
 	
-	public SON(int k_, int dimension_, double lr_, int epochs_)
+	public SON(String parametersFilename)
+	{
+		ReadParameters(parametersFilename);
+		neurons = new ArrayList<Neuron>();
+		for (int i = 0; i < k; i++)
+		{
+			Neuron neuron = new Neuron(i, dimension);
+			neurons.add(neuron);
+		}
+	}
+	
+	public SON(int k_, int dimension_, double lr_, int epochs_, int delayTime_)
 	{
 		k = k_;
 		dimension = dimension_;
 		lr = lr_;
 		epochs = epochs_;
+		delayTime = delayTime_;
 		neurons = new ArrayList<Neuron>();
 		for (int i = 0; i < k; i++)
 		{
@@ -52,14 +66,15 @@ public class SON
 	{
 		points = new ArrayList<Point>();
 		String dataline = null;
-        try {
+        try 
+        {
             // FileReader reads text files in the default encoding.
             FileReader fileReader = 
                 new FileReader(pointsFilename);
 
             // Always wrap FileReader in BufferedReader.
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-
+            
             while((dataline = bufferedReader.readLine()) != null) 
             {
             	String items[]= dataline.split(",");
@@ -83,10 +98,58 @@ public class SON
         }	
 	}
 	
+	public void ReadParameters(String parametersFilename)
+	{
+		points = new ArrayList<Point>();
+		String dataline = null;
+		boolean first = true;
+        try
+        {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(parametersFilename);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((dataline = bufferedReader.readLine()) != null) 
+            {
+            	if (first)
+            	{
+            		first = false;
+            		continue;
+            	}
+            	String items[]= dataline.split(",");
+            	k = Integer.parseInt(items[0]);
+            	dimension = Integer.parseInt(items[1]);
+            	lr = Double.parseDouble(items[2]);
+            	epochs = Integer.parseInt(items[3]);
+            	delayTime = Integer.parseInt(items[4]);
+            }
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) 
+        {
+        	System.out.println("Unable to open file '" +  parametersFilename + "'"); 
+        }  
+        catch(IOException ex)
+        {
+            System.out.println("Error reading file '" + parametersFilename + "'");  
+        }
+	}
+	
 	public void Train()
 	{
+		JFrame frame = new JFrame("Clustering...");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1000, 1000);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 		for (int epochID = 0; epochID < epochs; epochID++)
 		{
+			classificationMatrix = new HashMap<Integer, Integer>();
 			for (int i = 0; i < points.size(); i++)
 			{
 				Point point = points.get(i);
@@ -103,6 +166,18 @@ public class SON
 					}
 				}
 				neurons.get(minID).UpdateWeights(point, lr);
+				classificationMatrix.put(point.GetID(), minID);
+			}
+			PointsDrawer pointsDrawer = new PointsDrawer(neurons, points, classificationMatrix);
+			frame.add(pointsDrawer);
+			frame.repaint();
+			frame.revalidate();
+			try
+			{
+				TimeUnit.SECONDS.sleep(delayTime);
+			} catch (InterruptedException e)
+			{
+				e.printStackTrace();
 			}
 		}
 	}
@@ -145,18 +220,15 @@ public class SON
 	public static void main(String args[])
 	{
 		String pointsFilename = Paths.get(".").toAbsolutePath().normalize().toString()+"\\data.csv";
-		int k = 2;
-		int dimension = 2;
-		double lr = 0.01;
-		int epochs = 5000;
-		SON son = new SON(k, dimension, lr, epochs);
+		String parametersFilename = Paths.get(".").toAbsolutePath().normalize().toString()+"\\parameters.csv";
+		SON son = new SON(parametersFilename);
 		son.ReadPoints(pointsFilename);
 		son.Train();
 		son.Classify();
 		//son.PrintClassification();
 		
 		PointsDrawer pointsDrawer = new PointsDrawer(son.GetNeurons(), son.GetPoints(), son.GetClassificationMatrix());
-		JFrame frame = new JFrame("Points to cluster");
+		JFrame frame = new JFrame("Results");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(pointsDrawer);
 		frame.setSize(1000, 1000);
